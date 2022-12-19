d9d38e42d5c1f28e7b64b9a07dcd10c8712569e1
Emracabe <caberemmar@gmail.com>
2022-12-19 20:36:14 +0800

    End of windows practice

--- windows.py
+++ windows.py
@@ -4,5 +4,11 @@ from tkinter import *
 # windows = serves as a container to hold or contain these widgets
 
 window = Tk() # instantiate an instance of a window
+window.geometry("420x420") # set the size of the window
+window.title("Emmar First Code GUI Program") # set the title of the window
 
-window.mainloop()
+icon = PhotoImage(file='logo.png') # initialize the icon to be used
+window.iconphoto(True, icon) # set the icon of the window
+window.config(background='#00FFFF') # set the background color
+
+window.mainloop() # place window on computer screen, listen for events
