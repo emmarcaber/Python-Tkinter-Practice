9c795d37ef5380b49ee6ff2db5a0fc46e06fd492
Emracabe <caberemmar@gmail.com>
2022-12-19 22:49:45 +0800

    Finished frames practice

---
+++ frames.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+window = Tk()
+
+frame = Frame(window,
+              bg="#00FFFF",
+              bd=5,
+              relief = SUNKEN
+)
+frame.place(x = 0, y = 0)
+
+Button(frame,
+            text="W",
+            font= ("Consolas", 25),
+            width = 3).pack(side=TOP)
+Button(frame,
+            text="A",
+            font= ("Consolas", 25),
+            width = 3).pack(side=LEFT)
+Button(frame,
+            text="S",
+            font= ("Consolas", 25),
+            width = 3).pack(side=LEFT)
+Button(frame,
+            text="D",
+            font= ("Consolas", 25),
+            width = 3).pack(side=LEFT)
+
+window.mainloop()
