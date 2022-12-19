1bd2acd09c63a9bedc81f55a58752c07ebdd9a51
Emracabe <caberemmar@gmail.com>
2022-12-19 20:27:48 +0800

    Creating first window

---
+++ windows.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+# widgets = GUI elements: buttons, textboxes, labels, images
+# windows = serves as a container to hold or contain these widgets
+
+window = Tk() # instantiate an instance of a window
+
+window.mainloop()
